586a44cf6f70b1d2a027416ba5abd1bf8a99ae94
Afzal Mohammed <afzal@ti.com>
2010-12-16 19:22:37 +0530

    FASTBOOT: Housekeeping
    Housekeeping
    
    Signed-off-by: Afzal Mohammed <afzal@ti.com>
    Signed-off-by: Amit Pundir <amitpundir@ti.com>

--- common/cmd_fastboot.c
+++ common/cmd_fastboot.c
@@ -1,8 +1,7 @@
 /*
  * Copyright (C) 2010 Texas Instruments
  *
- * Author :
- *	Mohammed Afzal M A <afzal@ti.com>
+ * Author : Mohammed Afzal M A <afzal@ti.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
