4048d1255b6067637e607ade5d000fb285d2fdd5
Mike J. Chen <mjchen@google.com>
2011-08-23 09:24:04 -0700

    ARMV7: Tungsten: Remove CONFIG_REVISION_TAG
    Kernel gets board revision itself from the gpio lines
    so don't pass it through ATAGS.
    
    Change-Id: Ie3eb3d44965465630a704f536d997eff1b977715
    Signed-off-by: Mike J. Chen <mjchen@google.com>
    Signed-off-by: John Grossman <johngro@google.com>

--- include/configs/omap4_tungsten.h
+++ include/configs/omap4_tungsten.h
@@ -65,7 +65,6 @@
 #define CONFIG_CMDLINE_TAG		1	/* enable passing of ATAGs */
 #define CONFIG_SETUP_MEMORY_TAGS	1
 #define CONFIG_INITRD_TAG		1
-#define CONFIG_REVISION_TAG		1
 
 #define CONFIG_OF_LIBFDT		1
 
