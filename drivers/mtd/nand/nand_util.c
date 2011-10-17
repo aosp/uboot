f6a9ebae3e5a4c4d396f0533a935ac2b6d31e34f
Scott Anderson <saa@google.com>
2011-10-17 15:10:26 -0700

    MTD NAND: Fix NAND YAFFS OOB handling.
    The handling of the WITH_YAFFS_OOB flag in nand_write_skip_bad()
    was flawed in two ways:
    
    1) The code that does the nand_write before the while loop can
    not be used in the case of WITH_YAFFS_OOB because it does not
    write the OOB data.  I fixed the flags check to skip the code if
    WITH_YAFFS_OOB was set.
    
    2) After #1 was fixed, it was observed that only the first block
    was written.  The flaw is that the sense of the check for the
    return status of nand->write_oob was reversed.  It would break
    out of the loop if it succeeded so only the first block was
    written.
    
    Change-Id: I89a5f6afd698ae74da9bdc136e81e3c00a21eaa5
    Signed-off-by: Scott Anderson <saa@google.com>

--- drivers/mtd/nand/nand_util.c
+++ drivers/mtd/nand/nand_util.c
@@ -528,7 +528,7 @@ int nand_write_skip_bad(nand_info_t *nand, loff_t offset, size_t *length,
 		return -EINVAL;
 	}
 
-	if (!need_skip && !(flags & WITH_DROP_FFS)) {
+	if (!need_skip && !(flags & (WITH_DROP_FFS | WITH_YAFFS_OOB))) {
 		rval = nand_write (nand, offset, length, buffer);
 		if (rval == 0)
 			return 0;
@@ -577,7 +577,7 @@ int nand_write_skip_bad(nand_info_t *nand, loff_t offset, size_t *length,
 				ops.oobbuf = ops.datbuf + pagesize;
 
 				rval = nand->write_oob(nand, offset, &ops);
-				if (!rval)
+				if (rval)
 					break;
 
 				offset += pagesize;
