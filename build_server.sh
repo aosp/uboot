5dba692d0d8b9437ca1fbc6a62bb036f1d8288e9
Ying Wang <wangying@google.com>
2011-10-25 15:32:18 -0700

    Add build script to be used by the build server.
    Bug: 5448433
    Change-Id: I5dc1ae52ee30cf32d54c6d6b348a187a36cc78d3

---
+++ build_server.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# This build script is used by the build server.
+# It's supposed to be executed in the directory where it lives.
+
+set -x -e
+
+if [ -z $BUILD_CONFIG ]; then
+  echo "Please set BUILD_CONFIG!"
+  exit 2
+fi
+
+if [ -z $BUILD_OUTPUT ]; then
+  echo "Please set output dir BUILD_OUTPUT!"
+  exit 2
+fi
+
+if [ -z $BUILD_JOBS ]; then
+  BUILD_JOBS=10
+fi
+
+FILES_TO_ARCHIVE="u-boot.bin MLO"
+if [ "$BUILD_CONFIG" = "omap4_tungsten_config" ]; then
+FILES_TO_ARCHIVE="$FILES_TO_ARCHIVE spl/u-boot-spl.bin"
+fi
+
+export ARCH=arm
+export CROSS_COMPILE=arm-eabi-
+
+make $BUILD_CONFIG
+make -j $BUILD_JOBS
+mkdir -p $BUILD_OUTPUT
+cp -f $FILES_TO_ARCHIVE $BUILD_OUTPUT
+
