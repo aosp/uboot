e46a4cd3cf974bea2092c86dc2fa8823c50094bf
Mike J. Chen <mjchen@google.com>
2011-11-08 12:27:26 -0800

    ARMV7: tungsten: Bump version to AB0001 (Nov, 2011, first build)
    Change-Id: I30b8e58dd8bac75d80c13ede26b2c459f7e6c511
    Signed-off-by: Mike J. Chen <mjchen@google.com>

--- include/configs/omap4_tungsten.h
+++ include/configs/omap4_tungsten.h
@@ -159,7 +159,7 @@
 #define	CONFIG_CMD_FASTBOOT
 #define	CONFIG_FASTBOOT_TRANSFER_BUFFER		(OMAP44XX_DRAM_ADDR_SPACE_START + SZ_16M)
 #define	CONFIG_FASTBOOT_TRANSFER_BUFFER_SIZE	(SZ_512M - SZ_16M)
-#define CONFIG_FASTBOOT_VERSION_BOOTLOADER "steelheadAA003"
+#define CONFIG_FASTBOOT_VERSION_BOOTLOADER "steelheadAB001"
 /* Fastboot product name */
 #define	FASTBOOT_PRODUCT_NAME	"steelhead"
 /* Fastboot reboot paramenter address, it's currently put at
