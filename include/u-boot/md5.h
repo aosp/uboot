1cf1d2e463a993a7aed46624bb00a200e36165d2
Scott Anderson <saa@android.com>
2012-02-08 16:14:39 -0800

    MD5 LIBRARY: Expose functions to do piecemeal MD5 calculation
    The functions MD5Init, MD5Update and MD5Final are implemented in
    lib/md5.c but they were static to that file.  This change
    promotes them to the global name-space so they can be used in
    code that wants to do an MD5 across more data than can fit in
    memory at one time.
    
    Change-Id: Ia9d73732268f6417ce267e4ea6dfb7e479ca1124
    Signed-off-by: Scott Anderson <saa@android.com>

--- include/u-boot/md5.h
+++ include/u-boot/md5.h
@@ -28,4 +28,19 @@ void md5 (unsigned char *input, int len, unsigned char output[16]);
 void md5_wd (unsigned char *input, int len, unsigned char output[16],
 		unsigned int chunk_sz);
 
+/*
+ * Start MD5 accumulation.  Set bit count to 0 and buffer to mysterious
+ * initialization constants.
+ */
+void MD5Init(struct MD5Context *ctx);
+/*
+ * Update context to reflect the concatenation of another buffer full
+ * of bytes.
+ */
+void MD5Update(struct MD5Context *ctx, unsigned char const *buf, unsigned len);
+/*
+ * Final wrapup - pad to 64-byte boundary with the bit pattern
+ * 1 0* (64-bit count of bits processed, MSB-first)
+ */
+void MD5Final(unsigned char digest[16], struct MD5Context *ctx);
 #endif /* _MD5_H */
